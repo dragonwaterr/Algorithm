4f505ddfbf04d52c1ef4516c53d74f1c08ca25d2
dragonwaterr <83999626+dragonwaterr@users.noreply.github.com>
2024-11-22 17:46:12 +0900

    [Gold I] Title: 트리의 순회, Time: 416 ms, Memory: 53292 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -8,6 +8,7 @@ public class Main {
     static StringBuilder sb;
     static int[] inorder;
     static int[] postorder;
+    static int[] inorderIndex;
     static int n;
 
     static class Node {
@@ -19,47 +20,39 @@ public class Main {
     static Node atchSub(Node node, int postRootIndex, int start, int end) {
         
         // 좌우 서브트리 크기 계산
-        int inRootIndex = 0;
-        for(int i = start; i < end; i++) {
-            if(inorder[i] == postorder[postRootIndex]) {
-                inRootIndex = i;
-                break;
-            }
-        }
+        int inRootIndex = inorderIndex[postorder[postRootIndex]];
         int leftSize = inRootIndex - start;
         int rightSize = end - inRootIndex - 1;
         
         // 노드에 값 넣기 
         node.data = inorder[inRootIndex];
-
+        sb.append(node.data).append(" ");
+        
+        // 왼쪽 서브트리의 루트 붙이기
+        if(leftSize != 0) { 
+            node.left = atchSub(new Node(), postRootIndex - rightSize - 1, inRootIndex - leftSize, inRootIndex);
+        }
+        
         // 오른쪽 서브트리의 루트 붙이기
         if(rightSize != 0) { 
             node.right = atchSub(new Node(), postRootIndex-1, inRootIndex + 1, inRootIndex + rightSize + 1);
         }
 
-        // 왼쪽 서브트리의 루트 붙이기
-        if(leftSize != 0) { 
-            node.left = atchSub(new Node(), postRootIndex - rightSize - 1, inRootIndex - leftSize, inRootIndex);
-        }
-
         return node;
     }
 
-    static void makePreorder(Node node) {
-        sb.append(node.data).append(" ");
-        if(node.left != null) makePreorder(node.left);
-        if(node.right != null) makePreorder(node.right);
-    }
-
     public static void main(String[] args) throws IOException {
+        sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         inorder = new int[n];
         postorder = new int[n];
-
+        inorderIndex = new int[n+1];
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
             inorder[i] = Integer.parseInt(st.nextToken());
+            inorderIndex[inorder[i]] = i;
         }
 
         st = new StringTokenizer(br.readLine());
@@ -69,9 +62,6 @@ public class Main {
         
         Node root = new Node();
         atchSub(root, n-1, 0, n);
-        
-        sb = new StringBuilder();
-        makePreorder(root);
         System.out.println(sb.toString());
     }
 }
