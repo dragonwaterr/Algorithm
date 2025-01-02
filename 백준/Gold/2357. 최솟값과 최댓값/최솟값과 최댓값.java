0d22384b7c88e72d9a8a2f7120a41726e2973242
dragonwaterr <83999626+dragonwaterr@users.noreply.github.com>
2025-01-02 20:35:01 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 684 ms, Memory: 76620 KB -BaekjoonHub

---
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    // 최댓값과_최솟값
+    static final int MAX = 1_000_000_001;
+    static StringBuilder sb;
+    static int[] minSegtree;
+    static int[] maxSegtree;
+
+    public static void main(String[] args) throws IOException {
+        sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        int firstLeaf = (int) Math.pow(2, Math.ceil(Math.log(n) / Math.log(2)));
+        int treeSize = 2 * firstLeaf;
+        minSegtree = new int[treeSize];
+        maxSegtree = new int[treeSize];
+
+        // 리프 입력
+        for (int i = firstLeaf; i < firstLeaf + n; i++) {
+            maxSegtree[i] = minSegtree[i] = Integer.parseInt(br.readLine());
+        }
+        for(int i = firstLeaf + n; i < treeSize; i++) {
+            minSegtree[i] = MAX;
+        }
+
+        // 트리 완성
+        for (int i = firstLeaf - 1; i > 0; i--) {
+            minSegtree[i] = Math.min(minSegtree[2 * i], minSegtree[2 * i + 1]);
+            maxSegtree[i] = Math.max(maxSegtree[2 * i], maxSegtree[2 * i + 1]);
+        }
+
+        // 쿼리 처리
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            if(from == to) {
+                sb.append(maxSegtree[from + firstLeaf - 1]).append(" ")
+                    .append(maxSegtree[from + firstLeaf - 1]).append("\n");
+                continue;
+            }
+            answerQuery(from + firstLeaf - 1, to + firstLeaf - 1);
+        }
+        br.close();
+        System.out.println(sb);
+    }
+
+    static void answerQuery(int from, int to) {
+        int min = MAX;
+        int max = -1;
+
+        while (from <= to) {
+            if (from % 2 == 1) {
+                min = Math.min(min, minSegtree[from]);
+                max = Math.max(max, maxSegtree[from]);
+                from++;
+            }
+            if (to % 2 == 0) {
+                min = Math.min(min, minSegtree[to]);
+                max = Math.max(max, maxSegtree[to]);
+                to--;
+            }
+            from /= 2;
+            to /= 2;
+        }
+        sb.append(min).append(" ").append(max).append("\n");
+    }
+}
