e69c839291d46691490584bea416716713995c16
dragonwaterr <83999626+dragonwaterr@users.noreply.github.com>
2025-01-16 23:10:28 +0900

    [Platinum V] Title: 임계경로, Time: 380 ms, Memory: 49688 KB -BaekjoonHub

--- 백준/Platinum/1948. 임계경로/임계경로.java
+++ 백준/Platinum/1948. 임계경로/임계경로.java
@@ -1,95 +1,86 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    // 임계경로
-    static int n, m;
-    static int start, finish;
-    static ArrayList<HashMap<Integer, Integer>> forward;
-    static ArrayList<HashMap<Integer, Integer>> reverse;
-    static int[] dist; // 각 도시까지의 최대 비용
+    static int n, m, start, finish;
+    static ArrayList<Node>[] forward;
+    static ArrayList<Node>[] reverse;
 
-    static class Buddy implements Comparable<Buddy> {
+    static class Node implements Comparable<Node> {
         int city, time;
-        public Buddy(int city, int time) {
-            this.city = city; // 현재 도시
-            this.time = time; // 누적 시간
+        public Node(int city, int time) {
+            this.city = city;
+            this.time = time;
         }
-        @Override
-        public int compareTo(Buddy o) {
-            return o.time - this.time;
+        public int compareTo(Node o) {
+            return Integer.compare(o.time, this.time); 
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        dist = new int[n + 1];
-        forward = new ArrayList<>(n + 1);
-        reverse = new ArrayList<>(n + 1);
+        m = Integer.parseInt(br.readLine());
+
+        int[] dist = new int[n + 1];
+        int[] indegree = new int[n + 1];
+        forward = new ArrayList[n + 1];
+        reverse = new ArrayList[n + 1];
+
         for (int i = 0; i <= n; i++) {
-            forward.add(new HashMap<>());
-            reverse.add(new HashMap<>());
+            forward[i] = new ArrayList<Node>();
+            reverse[i] = new ArrayList<Node>();
         }
 
-        m = Integer.parseInt(br.readLine());
         for (int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
-            int former = forward.get(from).getOrDefault(to, 0);
-            forward.get(from).put(to, Math.max(forward.get(from).getOrDefault(to, 0), weight));
-            reverse.get(to).put(from, Math.max(reverse.get(to).getOrDefault(from, 0), weight));
+            forward[from].add(new Node(to, weight));
+            reverse[to].add(new Node(from, weight));
+            indegree[to]++;
         }
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         start = Integer.parseInt(st.nextToken());
         finish = Integer.parseInt(st.nextToken());
-        
-        // 다익스트라 : 최대 시간 계산
-        Buddy buddy = new Buddy(start, 0);
-        PriorityQueue<Buddy> pq = new PriorityQueue<>();
-        pq.add(buddy);
-        while (!pq.isEmpty()) {
-            Buddy cur = pq.poll();
 
-            if(cur.time < dist[cur.city]) continue;
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(start);
 
-            for (Map.Entry<Integer, Integer> next : forward.get(cur.city).entrySet()) {
-                int nextCity = next.getKey();
-                int nextTime = next.getValue();
+        while (!q.isEmpty()) {
+            int cur = q.poll();
 
-                if (dist[cur.city] + nextTime > dist[nextCity]) {
-                    dist[nextCity] = cur.time + nextTime;
-                    pq.add(new Buddy(nextCity, dist[nextCity]));
+            for (Node node : forward[cur]) {
+                indegree[node.city]--;
+                dist[node.city] = Math.max(dist[node.city], dist[cur] + node.time);
+                if (indegree[node.city] == 0) {
+                    q.offer(node.city);
                 }
             }
         }
 
-        // 역추적
-        Set<Integer> set = new HashSet<>(); // 임계경로 간선들을 저장
-        Queue<Buddy> q = new ArrayDeque<>();
-        q.add(new Buddy(finish, dist[finish]));
+        int edgeCount = 0;
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> qReverse = new ArrayDeque<>();
+        q.offer(finish);
+        visited[finish] = true;
+
         while (!q.isEmpty()) {
-            Buddy cur = q.poll();
-
-            for (Map.Entry<Integer, Integer> prev : reverse.get(cur.city).entrySet()) {
-                int prevCity = prev.getKey();
-                int prevTime = prev.getValue();
-
-                if (dist[cur.city] - prevTime == dist[prevCity]) {
-                    if (!set.contains(cur.city * 100000 + prevCity)) {
-                        set.add(cur.city * 100000 + prevCity);
-                        q.add(new Buddy(prevCity, dist[prevCity])); // 맞네.. 이거네
+            int cur =  q.poll();
+            for(Node prev : reverse[cur]) {
+                if(dist[prev.city] == dist[cur] - prev.time) {
+                    edgeCount++;
+                    if(!visited[prev.city]) {
+                        q.offer(prev.city);
+                        visited[prev.city] = true;
                     }
                 }
             }
         }
-        
+
         System.out.println(dist[finish]);
-        System.out.println(set.size());
+        System.out.println(edgeCount);
     }
 }
