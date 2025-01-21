0a0ebd721289df9c83c7fbc1db4b072f4f3f0908
dragonwaterr <83999626+dragonwaterr@users.noreply.github.com>
2025-01-21 16:21:10 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 320 ms, Memory: 48108 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    // 별 찍기 - 10
+    static int n, k;
+    static char[][] stars;
+    static int[][] dt = {{0, 1}, {0, 2}, {1, 0}, {1, 2}, {2, 0}, {2, 1}, {2, 2}};
+    static StringBuilder sb;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        
+        stars = new char[n+1][n+1];
+        for(int i = 1; i <= n; i++) {
+            Arrays.fill(stars[i], ' ');
+        }
+        
+        stars[1][1] = '*';
+        copy(1);
+        printStars();
+    }
+    
+    
+    static void copy(int curW) {
+        if(curW == n) return;
+        
+        for (int i = 0; i < 7; i++) {
+            int sx = 1 + curW * dt[i][0]; 
+            int sy = 1 + curW * dt[i][1]; 
+            
+            for(int j = sx; j < sx + curW; j++) { 
+                for(int k = sy; k < sy + curW; k++) { 
+                    stars[j][k] = stars[j - sx + 1][k - sy + 1];
+                }
+            }
+        }
+        
+        copy(curW*3);
+    }
+    
+    static void printStars() {
+        sb = new StringBuilder();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                sb.append(stars[i][j]);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
