2f7eaa3e53f029c8220b824acb3f0b07f3ed42b2
dragonwaterr <83999626+dragonwaterr@users.noreply.github.com>
2025-01-21 13:34:43 +0900

    [Silver II] Title: 종이의 개수, Time: 832 ms, Memory: 310020 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    // 종이의 개수
+    static int n;
+    static int[][] paper;
+    static int[] answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        paper = new int[n+1][n+1];
+        answer = new int[3];
+        for(int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= n; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        slice(n, 1, 1);
+        sb.append(answer[0]).append("\n").append(answer[1]).append("\n").append(answer[2]);
+        System.out.println(sb);
+    }
+
+    static void slice(int width, int sx, int sy) {
+        int pattern = paper[sx][sy];
+
+        for(int i = sx; i < sx + width; i++) {
+            for(int j = sy; j < sy + width; j++) {
+                
+                // 9 등분
+                if(paper[i][j] != pattern) { 
+                    int nextWidth = width/3;
+                    for(int k = 0; k < 3; k++) {
+                        for(int l = 0; l < 3; l++) {
+                            slice(nextWidth, sx + k*nextWidth, sy + l*nextWidth);
+                        }
+                    }
+                    return; // 9 등분을 했다면 현재 색종이 탐색은 종료
+                }
+
+            }
+        }
+
+        answer[pattern + 1]++;
+    }
+
+}
