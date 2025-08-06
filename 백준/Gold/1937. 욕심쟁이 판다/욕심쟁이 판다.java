8f79b73a1fa62730f807e1d57abd845d71794ecb
Yongsu Choi <83999626+dragonwaterr@users.noreply.github.com>
2025-08-06 17:32:07 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 488 ms, Memory: 37164 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -38,18 +38,12 @@ public class Main {
         if(dp[x][y] != 0) return dp[x][y];
         dp[x][y] = 1;
 
-        int curDp = dp[x][y];
         for (int i = 0; i < 4; i++) {
             int nx = x + dt[i][0];
             int ny = y + dt[i][1];
 
             if(nx < 0 || nx > n-1 || ny < 0 || ny > n-1) continue;
             if(map[x][y] >= map[nx][ny]) continue;
-            if(dp[nx][ny] != 0) {
-                dp[x][y] = Math.max(dp[x][y], curDp + dp[nx][ny]);
-                continue;
-            }
-
             dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
         }
 
