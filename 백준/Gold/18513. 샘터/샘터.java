46cc1d925a72b462c6e39116e32ad8da67f05d67
Yongsu Choi <83999626+dragonwaterr@users.noreply.github.com>
2025-08-13 02:10:14 +0900

    [Gold IV] Title: 샘터, Time: 504 ms, Memory: 55320 KB -BaekjoonHub

--- 백준/Gold/18513. 샘터/샘터.java
+++ 백준/Gold/18513. 샘터/샘터.java
@@ -1,17 +1,17 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.HashMap;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     // 샘터
-    
+
     static class Pointer {
-        int pos; 
-        int dist; 
-        boolean isLeft; 
+        int pos;
+        int dist;
+        boolean isLeft;
 
         Pointer(int pos, int dist, boolean isLeft) {
             this.pos = pos;
@@ -22,7 +22,7 @@ public class Main {
 
     static long answer;
     static HashMap<Integer, Character> obs; // 좌표 : 집 OR 샘터
-    static PriorityQueue<Pointer> pq;
+    static ArrayDeque<Pointer> dq;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,7 +30,7 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         obs = new HashMap<>();
-        pq = new PriorityQueue<>((o1, o2) -> o1.dist - o2.dist); // 샘터와의 거리 오름차순 정렬
+        dq = new ArrayDeque<>();
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
@@ -38,16 +38,16 @@ public class Main {
             obs.put(pos, 'S');
 
             if(obs.get(pos-1) == null) { // 왼쪽 포인터 생성
-                pq.add(new Pointer(pos-1, 1, true));
+                dq.add(new Pointer(pos - 1, 1, true));
             }
             if(obs.get(pos+1) == null) { // 오른쪽 포인터 생성
-                pq.add(new Pointer(pos+1, 1, false));
+                dq.add(new Pointer(pos + 1, 1, false));
             }
         }
 
         int cnt = 0;
         while(cnt < k) {
-            Pointer cur = pq.poll();
+            Pointer cur = dq.poll();
 
             int dest = cur.pos;
             int dist = cur.dist;
@@ -63,7 +63,7 @@ public class Main {
 
             int nextPos = dir ? dest-1 : dest+1;
             if(obs.get(nextPos) == null) {
-                pq.add(new Pointer(nextPos, dist+1, dir));   
+                dq.add(new Pointer(nextPos, dist + 1, dir));
             }
         }
 
