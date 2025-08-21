2e5ebe80d3e00ac30e3d188f163bc61f3f2f45fd
Yongsu Choi <83999626+dragonwaterr@users.noreply.github.com>
2025-08-21 10:51:43 +0900

    [Gold IV] Title: 회사 문화 1, Time: 300 ms, Memory: 31004 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -1,38 +1,72 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     // 회사 문화 1
     
+    static int nextInt() throws IOException {
+        int sign = 1;
+        int input;
+        while (!Character.isDigit(input = System.in.read())) {
+            if (input == '-') {
+                sign = -1;
+                input = System.in.read();
+                break;
+            }
+        }
+        int value = input & 15;
+        while (Character.isDigit(input = System.in.read())) {
+            value = value * 10 + (input & 15);
+        }
+        return sign * value;
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int n, m;
+        n = nextInt();
+        m = nextInt();
 
-        int[] parents = new int[n + 1];
-        st = new StringTokenizer(br.readLine());
+        int[] praise = new int[n+1];
+        ArrayList<Integer>[] subOrd = new ArrayList[n+1];
+
         for (int i = 1; i <= n; i++) {
-            parents[i] = Integer.parseInt(st.nextToken());
+            subOrd[i] = new ArrayList<>();
         }
 
-        int[] dp = new int[n + 1];
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int empNum = Integer.parseInt(st.nextToken());
-            int val = Integer.parseInt(st.nextToken());
-            dp[empNum] += val;
-        }
-
+        nextInt();
         for (int i = 2; i <= n; i++) {
-            dp[i] += dp[parents[i]];
+            int p = nextInt();
+            subOrd[p].add(i); 
+        }
+
+        for (int i = 0; i < m; i++) {
+            int empNum = nextInt();
+            int val = nextInt();
+            praise[empNum] += val;
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+
+        for (int i = 0; i < subOrd[1].size(); i++) {
+            int sub = (int)subOrd[1].get(i);
+            q.add(sub);
+        }
+
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            int pr = praise[cur];
+            if(!subOrd[cur].isEmpty()) {
+                for (int i = 0; i < subOrd[cur].size(); i++) {
+                    int sub =  (int)subOrd[cur].get(i);
+                    praise[sub] += pr;
+                    q.add(sub);
+                }
+            }
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
-            sb.append(dp[i]).append(" ");
+            sb.append(praise[i]).append(" ");
         }
         System.out.println(sb);
     }
