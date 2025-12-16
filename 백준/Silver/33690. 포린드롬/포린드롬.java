a12422ee3df67973c102c0a179feed966d0453a1
Yongsu Choi <83999626+dragonwaterr@users.noreply.github.com>
2025-12-16 21:18:20 +0900

    [Silver V] Title: 포린드롬, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Silver/33690. 포린드롬/포린드롬.java
+++ 백준/Silver/33690. 포린드롬/포린드롬.java
@@ -10,30 +10,24 @@ class Main {
             this.accum = accum;
         }
     }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         
         ArrayList<Pal> list = new ArrayList<>(); 
-        // 1~9 를 추가
         for(int i = 0; i < 10; i++) {
             list.add(new Pal(i, i+1));
         }
-   
         
         for(int i = 1; i < 9; i++) {
-            // 11~99, 111~999... 추가
-            // 0123456789 (10) 11 22 33 44 55 66 77 88 99
-            int level = (int)Math.pow(10, i); //10, 100, 1000 ... 1억
+            int level = (int)Math.pow(10, i); 
             for(int j = 1; j <= 9; j++) {
                 int prevNum = list.get(list.size()-9).num; 
                 int num = level*j + prevNum;
                 list.add(new Pal(num, list.get(list.size()-1).accum+1));
             }    
         }
-        
-        
-        
 
         for(int i = 0; i < list.size(); i++) {
             int cur = list.get(i).num; 
@@ -44,6 +38,5 @@ class Main {
         }
         
         System.out.println(82);
-
     }
 }
